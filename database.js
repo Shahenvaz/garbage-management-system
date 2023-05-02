fbe1d125c0a763564ef0916ef877db287f6d145c
Shahenvaz <ska78657@gmail.com>
2023-05-02 18:46:59 +0530

    added another commit with database.js modified with kubernetes url

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 const { MongoClient } = require('mongodb')
 
 class Connection {
-    url = "mongodb://127.0.0.1:27017"
+    url = "mongodb://192.168.49.2:30001/"
     client = new MongoClient(this.url)
     database = this.client.db('garbage-management-system')
     collection = this.database.collection('AdminDetails')
