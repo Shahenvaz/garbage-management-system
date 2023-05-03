86567d75214fa135c88e075c71ac3d9ab99beb2d
Shahenvaz <ska78657@gmail.com>
2023-05-03 15:08:33 +0530

    added metric files

--- index.js
+++ index.js
@@ -2,11 +2,38 @@ const express = require('express')
 const app = new express()
 require("dotenv").config()
 const {Connection}  = require('./database')
+
+const Prometheus = require('prom-client')
 const db =  new Connection
 
 app.use(express.urlencoded({ extended: false }))
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
+
+//graphana metric
+
+const client = require('prom-client');
+
+// Create a Registry to register the metrics
+const register = new client.Registry();
+client.collectDefaultMetrics({register});
+
+client.collectDefaultMetrics({
+    app: 'node-application-monitoring-app',
+    prefix: 'node_',
+    timeout: 10000,
+    gcDurationBuckets: [0.001, 0.01, 0.1, 1, 2, 5],
+    register
+});
+
+app.get('/metrics', async (req, res) => {
+    res.setHeader('Content-Type', register.contentType);
+    res.send(await register.metrics());
+});
+    
+
+
+//end metric
 
 app.get('/',(req,res)=>{
     res.render('index')
