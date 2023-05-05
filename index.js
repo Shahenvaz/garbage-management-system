77a1ee6301e9b8d37a07424abeff24405e91b82c
Shahenvaz <79762944+Shahenvaz@users.noreply.github.com>
2023-05-05 14:18:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.urlencoded({ extended: false }))
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
 
-//graphana metrics is working successfully
+//graphana metrics is working successfully workugn
 
 const client = require('prom-client');
 
