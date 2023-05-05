b1ad65846decb917792b0e3a1391e8672863b425
Shahenvaz <79762944+Shahenvaz@users.noreply.github.com>
2023-05-05 12:21:27 +0530

    changed shahenvaz to shahenvaz khan

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.urlencoded({ extended: false }))
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
 
-//graphana metricss is the best thing to do shahenvaz
+//graphana metricss is the best thing to do shahenvaz khan
 
 const client = require('prom-client');
 
