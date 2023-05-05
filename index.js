eb165001b0a288cbc19bccec53a2f6c56b8411cc
Shahenvaz <79762944+Shahenvaz@users.noreply.github.com>
2023-05-05 13:51:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.urlencoded({ extended: false }))
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
 
-//graphana metricss
+//graphana met
 
 const client = require('prom-client');
 
